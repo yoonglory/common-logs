6bfdf809cd4c37b046ef2a07c0d59110f289cff1
yoonglory <yoonglory@gmail.com>
2025-07-10 15:37:01 +0900

    test

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:3.10-slim
 WORKDIR /app
 COPY app.py .
 RUN pip install flask
+RUN pip install prometheus_client 
 ENV COLOR=blue
 CMD ["python", "app.py"]
 
--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
+
 app = Flask(__name__)
 
+# 메트릭 정의
+REQUEST_COUNT = Counter("http_requests_total", "Total HTTP Requests", ["method", "endpoint"])
+REQUEST_LATENCY = Histogram("http_request_duration_seconds", "Request latency", ["endpoint"])
+
 @app.route("/")
+@REQUEST_LATENCY.labels("/").time()
 def hello():
-    return "Hello from AWS EKS with ArgoCD!12"
+    REQUEST_COUNT.labels(method=request.method, endpoint="/").inc()
+    return "Hello from monitored Flask!"
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
+
